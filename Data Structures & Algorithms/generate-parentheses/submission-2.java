2b4e5866ca3e71b3da2bddc6b50919c3a820623d
arpithaga <34171932+arpithaga@users.noreply.github.com>
2026-04-05 11:54:30 -0700

    Add: generate-parentheses - submission-2

---
+++ Data Structures & Algorithms/generate-parentheses/submission-2.java
@@ -0,0 +1,32 @@
+class Solution {
+    List<String> result = new ArrayList<>();
+
+	public List<String> generateParenthesis(int n) {
+		result.clear();
+		dfs(n, new StringBuilder(), 0, 0);
+		return result;
+	}
+
+	private void dfs(int n, StringBuilder current, int open, int close) {
+
+        if(open>n || close>n) return;
+
+		if ((open==n && close==n) && current.length() == 2 * n) {
+			result.add(current.toString());
+			return;
+		}
+
+		if (open < n) {
+			current.append("(");
+			dfs(n, current, open + 1, close);
+			current.deleteCharAt(current.length() - 1);
+		}
+
+		if (close < open) {
+			current.append(")");
+			dfs(n, current, open, close + 1);
+			current.deleteCharAt(current.length() - 1);
+		}
+
+	}
+}
