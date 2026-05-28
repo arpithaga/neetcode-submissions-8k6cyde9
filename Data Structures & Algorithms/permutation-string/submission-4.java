acf157f49318831462daf222ae36a4480cb97ec3
arpithaga <34171932+arpithaga@users.noreply.github.com>
2026-05-27 18:19:44 -0700

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] counts=new int[26];
+        for(char c:s1.toCharArray()){
+            counts[c-'a']++;
+        }
+
+        int i=0,j=0;
+        while(j<s2.length()){
+            counts[s2.charAt(j)-'a']--;
+
+            while(counts[s2.charAt(j)-'a']<0){
+                counts[s2.charAt(i)-'a']++;
+                i++;
+            }
+
+            if(j-i+1==s1.length())
+               return true;
+
+            j++;
+
+        }
+
+        return false;
+    }
+}
