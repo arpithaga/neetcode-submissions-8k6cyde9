f828516510d864db16320f7500e6d0c73e9b27db
arpithaga <34171932+arpithaga@users.noreply.github.com>
2026-06-06 10:42:00 -0700

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] r = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+        for(int i=temperatures.length-1;i>=0;i--){
+            while(!stack.isEmpty() && temperatures[stack.peek()] <= temperatures[i]){
+                stack.pop();
+            }
+            if(!stack.isEmpty())
+              r[i] = stack.peek()-i;
+            stack.push(i);
+        }
+        
+        return r;
+    }
+}
