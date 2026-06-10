c3761e5a686851d10aebfadace2f89907eb51fca
arpithaga <34171932+arpithaga@users.noreply.github.com>
2026-06-09 17:41:05 -0700

    Add: car-fleet - submission-13

---
+++ Data Structures & Algorithms/car-fleet/submission-13.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+
+        if(position==null || position.length==0) return 0;
+
+        int[][] pair = new int[position.length][2];
+        for(int i=0;i<position.length;i++){
+            pair[i][0]=position[i];
+            pair[i][1]=speed[i];
+        }
+
+        Arrays.sort(pair,(a,b)->Integer.compare(b[0],a[0]));
+
+        int fleets=1;
+        double prevTime= (double)(target-pair[0][0])/pair[0][1];
+        for(int i=1;i<pair.length;i++){
+            double currentTime=  (double)(target-pair[i][0])/pair[i][1];
+
+            if(currentTime>prevTime){
+                fleets++;
+                prevTime=currentTime;
+            }
+        }
+
+        return fleets;
+
+    }
+}
