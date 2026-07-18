a7201b8b1d49ccb1e1782c69c04f92dec3eac57a
arpithaga <34171932+arpithaga@users.noreply.github.com>
2026-07-17 18:59:32 -0700

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,11 @@
+class Solution {
+
+	int[] memo = new int[46];
+    public int climbStairs(int n) {
+		if(memo[n]>0) return memo[n];
+		if(n==0) return 0;
+		if(n==1) return 1;
+		if(n==2) return 2;
+        return memo[n]=climbStairs(n-1)+climbStairs(n-2);
+    }
+}
